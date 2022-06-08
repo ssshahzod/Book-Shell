8842919c47a4921a0cbecfb0a1399c0352a92b52
Cronion <ohmytarik@mail.ru>
2022-06-08 19:34:26 +0300

    initial commit

---
+++ src/main/java/com/studying/bookshell/BookShellApplication.java
@@ -0,0 +1,13 @@
+package com.studying.bookshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookShellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studying/bookshell/BookShellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studying.bookshell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
